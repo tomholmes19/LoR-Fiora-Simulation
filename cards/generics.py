74da2d62942327d97aeb67f08d83654c098c0992
Tom Holmes <s2070797@ed.ac.uk>
2021-06-14 14:50:14 +0100

    upload files

---
+++ cards/generics.py
@@ -0,0 +1,56 @@
+import random
+
+from card_containers import *
+
+class Card:
+    def __init__(self, name, cost) -> None:
+        self.name = name
+        self.cost = cost
+    
+    def __str__(self) -> str:
+        return (
+            "Name:\t {}\n".format(self.name) +
+            "Cost:\t {}".format(self.cost)
+        )
+
+class Spell(Card):
+    def __init__(self, name, cost, speed) -> None:
+        super().__init__(name, cost)
+        self.speed = speed
+    
+    def __str__(self) -> str:
+        return (
+            super().__str__() + "\n" +
+            "Speed:\t {}".format(self.speed)
+        )
+
+
+
+class Unit(Card):
+    def __init__(self, name, cost, power, health) -> None:
+        super().__init__(name, cost)
+        self.power = power
+        self.health = health
+    
+    def __str__(self) -> str:
+        return (
+            super().__str__() + "\n" +
+            "Power:\t {}\n".format(self.power) +
+            "Health:\t {}".format(self.health)
+        )
+
+class Champion(Unit):
+    def __init__(self, name, cost, power, health, level=0) -> None:
+        super().__init__(name, cost, power, health)
+        self.level = level
+    
+    def __str__(self) -> str:
+        return (
+            super().__str__() + "\n" +
+            "Level:\t {}".format(self.level)
+        )
+
+    
+class Follower(Unit):
+    def __init__(self, name) -> None:
+        super().__init__(name)
