552c1047dc06bd774708bf93d3138f512edda4a2
Tom Holmes <s2070797@ed.ac.uk>
2021-06-18 17:42:26 +0100

    improve simulation model

--- sim.py
+++ sim.py
@@ -1,40 +1,48 @@
+from cards import entreat
 import cards
 import card_containers
 import utils
 
-def simulation():
-    # Step 1: Initialize variables
-    card_dict = {
-        cards.generics.Card(name="_", cost=0): 34,
-        cards.fiora.Fiora(): 3,
-        cards.entreat.Entreat(): 3
-    }
+def simulation(cards_to_draw, deck_size=40, mulligan_size=4):
+    """
+    Runs a single simulation to find the number of draws needed to draw a card type in cards_to_draw,
+    a dictionary of the format
+        <Card>: number of this card
 
+    Input:
+        cards_to_draw (dict): dictionary of cards to draw for in the simulation
+    
+    Return:
+        current_draw (int): the draw on which a card in cards_to_draw is drawn
+    """
+    # Initialize variables
+
+    non_generics = sum([num for num in cards_to_draw.values()])
+    generics = deck_size - non_generics
+
+    card_dict = {cards.generics.Card(name="_", cost=0): generics} | cards_to_draw
     deck = utils.deck_builder(card_dict)
+
     hand = card_containers.Hand()
 
     current_draw = 0
 
-    # Step 2: Simulate game
-    while not utils.in_hand(hand, "Fiora", "Entreat"):
-        # Mulligan round
-        if current_draw in [0, 4]:
-            # If current_draw==4, i.e. drawn 4 generics, replace all cards in hand
-            if current_draw == 4:
-                hand.replace_all(deck)
-
-            # Draw 4 cards
-            for _ in range(4):
-                current_draw = utils.sim_draw(current_draw, hand, deck)
-
-                # break loop if previously drawn card is Fiora or Entreat
-                if hand[-1].name in ["Fiora", "Entreat"]:
-                    break
+    # Simulate game
+    while not utils.in_hand(hand, ["Fiora", "Entreat"]):
+        # Mulligan
+        if current_draw == 2*mulligan_size:
+            for _ in range(mulligan_size):
+                # index=0 ensures the 4 first cards added into hand are replaced
+                hand.replace(deck, index=0, shuffle=False)
+                deck.shuffle()
         
-        # Standard round
-        else:
-            current_draw = utils.sim_draw(current_draw, hand, deck)
+        current_draw += 1
+        hand.draw(deck)
 
     return current_draw
 
-print(simulation())
+cards_to_draw = {
+    cards.fiora.Fiora(): 3,
+    cards.entreat.Entreat(): 3
+}
+print(simulation(cards_to_draw, deck_size=50, mulligan_size=5))
--- utils.py
+++ utils.py
@@ -1,21 +1,20 @@
 import cards
 import card_containers
 
-def in_hand(hand, *args) -> bool:
+def in_hand(hand, card_list) -> bool:
     """
-    Checks if a card with name specified in *args is in hand
+    Checks if a card with name specified in card_list is in hand
 
     Input:
         hand (Hand): the hand to check
-        *args (tuple): tuple of card names to check
+        card_list (list): list of card names to check
     
     Return:
         in_hand (bool): True if card type in hand, false otherwise
     """
-
     in_hand = False
     for card in hand:
-        if card.name in args:
+        if card.name in card_list:
             in_hand = True
             break
     
@@ -45,6 +44,7 @@ def deck_builder(card_dict, shuffle=True):
 
     return deck
 
+
 if __name__ == "__main__":
     card_dict = {
         cards.generics.Card(name="_", cost=0): 34,
@@ -54,8 +54,3 @@ if __name__ == "__main__":
 
     deck = deck_builder(card_dict)
     print(deck)
-
-def sim_draw(current_draw, hand, deck) -> int:
-    draw = current_draw + 1
-    hand.draw(deck)
-    return draw
